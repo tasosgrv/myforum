d35527d7374c59fc6a3e8c347f51b9e57803e353
sakafliasg4 <tasosgr4@gmail.com>
2016-12-19 04:04:45 +0200

    adds class table-responsive

--- main_section.php
+++ main_section.php
@@ -11,14 +11,16 @@
         <section id="main">
             <div class="panel panel-primary">
                 <div class="panel-heading"> Θέματα</div>
-                <table class="table table-hover">
-                    <tr>
-                        <td>#</td><td>Τιτλος</td><td>Μηνύματα</td><td>Τελευαίος Χρηστης</td><td>Τελευταία ανανέωση</td>
-                    </tr>
-                    <tr>
-                        <td>1</td><td>ασδξκφηλακσδξφηλασδκξφηλακσδξφληακσδξφη</td><td>12</td><td>tasosg4</td><td>15/12/2016</td>
-                    </tr>
-                </table>
+                <div class="table-responsive">
+                    <table class="table table-hover">
+                        <tr>
+                            <td>#</td><td>Τιτλος</td><td>Μηνύματα</td><td>Τελευαίος Χρηστης</td><td>Τελευταία ανανέωση</td>
+                        </tr>
+                        <tr>
+                            <td>1</td><td>ασδξκφηλακσδξφηλασδκξφηλακσδξφληακσδξφη</td><td>12</td><td>tasosg4</td><td>15/12/2016</td>
+                        </tr>
+                    </table>
+                </div>
             </div>    
         </section>                
     </div>
