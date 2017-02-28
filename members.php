4613db4ba23c92ecae8c03c5ac3461b8687235e4
sakafliasg4 <tasosgr4@gmail.com>
2017-02-28 21:40:08 +0200

    fix for modals 2

--- members.php
+++ members.php
@@ -135,7 +135,6 @@
     <?php
         include('footer.php');
     ?>
-    <!-- Latest compiled and minified JavaScript -->
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+
 </body>
 </html>
--- subject.php
+++ subject.php
@@ -116,8 +116,6 @@
     <?php
         include('footer.php');
     ?>
-        <!-- Latest compiled and minified JavaScript -->
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
-    <script src="http://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>	<!-- import jquery lib -->
+
 </body>
 </html>
